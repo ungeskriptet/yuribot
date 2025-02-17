1619ec39993edc608c97fa63c30b9cb26dcd8b57
David Wronek <david@mainlining.org>
2025-02-17 16:06:41 +0100

    Improve GIF quality

--- yuribot/gifconvert.py
+++ yuribot/gifconvert.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def convert_gif() -> str:
-    command = f'ffmpeg -y -i temp/video.mp4 -filter_complex scale=320:-1 -f gif temp/animation.gif'
+    command = f'ffmpeg -y -i temp/video.mp4 -vf "scale=-1:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" temp/animation.gif'
     ffmpeg_cmd = subprocess.run(
         shlex.split(command),
         stdin=subprocess.PIPE,
